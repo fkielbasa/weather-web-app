c052de4e5431b81b981ab85119d43caa50bcc14a
DwiN3 <dwinicht333@gmail.com>
2023-12-06 20:03:56 +0100

    dodanie obu widoków

--- scripts/display.js
+++ scripts/display.js
@@ -1,12 +1,23 @@
 function display_mode(mode) {
-    const day = document.getElementById('day_button');
-    const week = document.getElementById('week_button');
+    const dayButton = document.getElementById('day_button');
+    const weekButton = document.getElementById('week_button');
+    const dayContent = document.getElementById('day_content');
+    const weekContent = document.getElementById('week_content');
+    const gridContent = document.getElementById('weather_main_content');
 
     if (mode === 'day') {
-        day.style.color = '#aaccec';
-        week.style.color = '#898989';
+        dayButton.style.color = '#aaccec';
+        weekButton.style.color = '#898989';
+        dayContent.style.display = 'grid';
+        weekContent.style.display = 'none';
+        gridContent.style.gridTemplateColumns = 'repeat(6, 1fr)';
     } else {
-        week.style.color = '#aaccec';
-        day.style.color = '#898989';
+        weekButton.style.color = '#aaccec';
+        dayButton.style.color = '#898989';
+        weekContent.style.display = 'grid';
+        dayContent.style.display = 'none';
+        gridContent.style.gridTemplateColumns = 'repeat(4, 1fr)';
     }
 }
+
+display_mode('day');
