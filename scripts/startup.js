5d2b99dc4e1b7fe0ad154b548789c03274c252d6
DwiN3 <dwinicht333@gmail.com>
2024-01-17 09:49:10 +0100

    usunięcie

--- scripts/startup.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-    const langTogglePL = document.querySelector('input[value="pl"]');
-    langTogglePL.checked = true;
-    changeLanguage('pl');
-    changeTemperature('C');
-    display_mode('day');
-    infoContent.style.display = 'none';
-};
